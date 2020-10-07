1d6d8d9534ce1bfb8e5d021c0a41b036ba8fbd21
daviddicken <davidfromseattle@gmail.com>
2020-10-07 13:09:27 -0700

    added and minked a css page to my home.html

--- src/main/java/com/daviddicken/codeFellowship/configs/WebSecurityConfig.java
+++ src/main/java/com/daviddicken/codeFellowship/configs/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers("/public/*").permitAll()
                 .antMatchers("/signup", "/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
